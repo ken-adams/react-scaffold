3bf5aef7c5a9bd687a87aa9792d6a449b216e30d
Stefan Lapcevic <stefanlapcevic@Stefans-MacBook-Pro.local>
2017-12-03 23:05:53 +0100

    Added new route

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './UI/Pages/HomePage';
+import ProfileSection from './UI/Components/ProfileSection';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
+        <Route path="/profile-section" component={ProfileSection} />
     </Route>
 );
