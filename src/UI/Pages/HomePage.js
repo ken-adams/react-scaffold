ab3697978c079913582991a17379114813083bdb
Stefan <stefan.lapcevic991@gmail.com>
2018-05-13 12:17:23 +0200

    Add features

--- src/UI/Pages/HomePage.js
+++ src/UI/Pages/HomePage.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 import * as homepageActions from "../../actions/homepageActions";
 
@@ -9,52 +9,58 @@ import MainButton from "../Components/MainButton";
 import SecondaryButton from "../Components/SecondaryButton";
 
 class HomePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
 
-	constructor(props, context) {
-		super(props, context);
-	}
-
-    render() {
-        return (
-            <div className="homepage">
-                <h1>Home Page</h1>
-                <p>This is homepage</p>
-                <label htmlFor="text">Plese enter text</label>
-                <input type="text" name="text" onChange={(e) => this.props.actions.homepageActions.setText(e.target.value)}/>
-                <button onClick={() => this.props.actions.homepageActions.postText(this.props.homepage.text)}>Post</button>
-                <button onClick={this.props.actions.homepageActions.get}>Get</button>
-				<br />
-                <div>{this.props.homepage.response["value"]}</div>
-                <div>
-                    <label htmlFor="selectTest">Label</label>
-                    <select name="selectTest" onChange={(e) => {
-                        console.log("value", e.target.value)
-                    }}>
-                        <option value={0}>Please choose</option>
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                    </select>
-                    </div>
-                <ProfileSection dummyFun={(valueFromChild) => {console.log("Value from child is: ", valueFromChild);}}/>
-                {/*<SecondaryButton text="Cancel"/>*/}
-            </div>
-
-        );
-    }
+  render() {
+    return (
+      <div className="homepage">
+        <h1>Home Page</h1>
+        <p>This is homepage</p>
+        <label htmlFor="text">Plese enter text</label>
+        <input type="text" name="text" onChange={e => this.props.actions.homepageActions.setText(e.target.value)} />
+        <button onClick={() => this.props.actions.homepageActions.postText(this.props.homepage.text)}>Post</button>
+        <button onClick={this.props.actions.homepageActions.get}>Get</button>
+        <br />
+        <div>{this.props.homepage.response["value"]}</div>
+        <div>
+          <label htmlFor="selectTest">Label</label>
+          <select
+            name="selectTest"
+            onChange={e => {
+              console.log("value", e.target.value);
+            }}
+          >
+            <option value={0}>Please choose</option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+          </select>
+        </div>
+        <br />
+        <ProfileSection
+          dummyFun={valueFromChild => {
+            console.log("Value from child is: ", valueFromChild);
+          }}
+        />
+        {/*<SecondaryButton text="Cancel"/>*/}
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state, ownProps) {
-	return {
-		homepage: state.homepage
-	};
+  return {
+    homepage: state.homepage
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		actions: {
-            homepageActions: bindActionCreators(homepageActions, dispatch)
-        }
-	};
+  return {
+    actions: {
+      homepageActions: bindActionCreators(homepageActions, dispatch)
+    }
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
--- src/actions/homepageActions.js
+++ src/actions/homepageActions.js
@@ -1,53 +1,56 @@
 import * as types from "./actionTypes";
 
 export function dummyAction(value) {
-    return {type: types.DUMMY_ACTION, value};
+  return { type: types.DUMMY_ACTION, value };
 }
 
 export function setText(value) {
-    return {type: types.SET_TEXT, value};
+  return { type: types.SET_TEXT, value };
 }
 
 export function saveResponse(value) {
-    return {type: types.SAVE_RESPONSE, value};
+  return { type: types.SAVE_RESPONSE, value };
 }
 
 export function postText(value) {
-    return dispatch => {
-        const data = {value};
-        return fetch("https://httpbin.org/post", 
-            {
-                method: "post",
-                headers: {
-                    "Content-Type":"application/json"
-                },
-                body: JSON.stringify(data)
-            }).then(response => {
-                return response.json();
-            }).then(json => {
-                dispatch(saveResponse(JSON.parse(json.data)));
-            }).catch(err => console.error("Error while doing post", err, err.stack));
-    };
+  return dispatch => {
+    const data = { value };
+    return fetch("https://httpbin.org/post", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        dispatch(saveResponse(JSON.parse(json.data)));
+      })
+      .catch(err => console.error("Error while doing post", err, err.stack));
+  };
 }
 
-export function get () {
-    return dispatch => {
-        return fetchData().then(response => {
-            console.log("URL from Response:", response.url);
-        }).catch(err => {
-            console.error("Error: ", err);
-        });
-    }
+export function get() {
+  return dispatch => {
+    return fetchData()
+      .then(response => {
+        console.log("URL from Response:", response.url);
+      })
+      .catch(err => {
+        console.error("Error: ", err);
+      });
+  };
 }
 
 async function fetchData() {
-    const data = await fetch("https://httpbin.org/get", 
-    {
-        method: "get",
-        headers: {
-            "Content-Type":"application/json"
-        }
-    });
-    const json = await data.json();
-    return json;
+  const data = await fetch("https://httpbin.org/get", {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  const json = await data.json();
+  return json;
 }
